31ce4eb80742328b0a4d2672b51d801558b7d846
patonw <4967614-patonw@users.noreply.gitlab.com>
2025-12-24 09:45:31 -0800

    Common dependencies

--- default.nix
+++ default.nix
@@ -2,38 +2,103 @@
   sources ? import ./nix/sources.nix,
   pkgs ? import sources.nixpkgs {},
   fenix ? import sources.fenix {},
-}:
-let
-  PLAYWRIGHT_ENV = ''
-      export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
-      export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
-  '';
-  libraries = with pkgs; [
-    openssl
-    wasmtime
-  ];
-  rustifer = fenix.combine [
-    # https://jordankaye.dev/posts/rust-wasm-nix/ but latest instead of stable
+  gitignore ? import sources."gitignore.nix" {},
+  nixgl ? import sources.nixGL {},
+  rust-toolchain ? fenix.combine [
     fenix.complete.toolchain
     fenix.targets.wasm32-unknown-unknown.latest.rust-std
+  ],
+  naersk ? pkgs.callPackage sources.naersk {
+    cargo = rust-toolchain;
+    rustc = rust-toolchain;
+  },
+}:
+let
+  libraries = with pkgs; [
+    stdenv.cc.cc.lib
+    xorg.libxcb
+    libxkbcommon
+    fontconfig
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXi
+    xorg.libX11.dev
+    libGL
+    zlib
+    openssl
+    wayland
+    dbus
   ];
-in
-{
-  shell = pkgs.mkShell {
-    LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath libraries}";
-    packages = with pkgs; [
-      niv
-      cmake
-      ninja
-      cargo-generate
-      moon
-      nodejs_22
+
+  callPackage = pkgs.lib.callPackageWith {
+    inherit sources pkgs fenix rust-toolchain naersk gitignore nixgl;
+    inherit (gitignore) gitignoreSource;
+  };
+  nixGL = nixgl.auto.nixGLDefault; # Necessary for running glutin on non-Nixos distros
+  build-aerie = { features ? [] } : naersk.buildPackage {
+    # Command line launchers
+    name = "aerie-bin";
+    gitSubmodules = true;
+    src = gitignore.gitignoreSource ./.;
+    cargoBuildOptions = opts: opts ++ [ "--package aerie" ] ++ (if features == [] then [] else [ "-F" (pkgs.lib.strings.join "," features)]);
+
+    nativeBuildInputs = with pkgs; [
       pkg-config
-      rustifer
-      uv
+      cmake
+      makeWrapper
+    ];
+
+    buildInputs = with pkgs; [
     ] ++ libraries;
-    shellHook = ''
-      ${PLAYWRIGHT_ENV}
+  };
+  aerie = build-aerie {};
+  migrate-aerie = build-aerie { features = ["migration"]; };
+in
+rec {
+  inherit pkgs libraries rust-toolchain;
+
+  aerie-bin = pkgs.writeShellApplication {
+    name = "aerie";
+    runtimeInputs = [nixGL aerie];
+    text = ''
+      export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}
+      nixGL ${aerie}/bin/aerie "$@"
     '';
+  };
+
+  aerie-runner = pkgs.writeShellApplication {
+    name = "aerie-runner";
+    runtimeInputs = [aerie];
+    text = ''
+      export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}
+      simple-runner "$@"
+    '';
+  };
+
+  migration = pkgs.writeShellApplication {
+    name = "aerie-migration";
+    runtimeInputs = [migrate-aerie];
+    text = ''
+      export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}
+      migrate-workflow "$@"
+    '';
+  };
+
+  desktop = pkgs.makeDesktopItem {
+    # Desktop launcher only
+    name = "Aerie";
+    desktopName = "Aerie Agentic Workflows";
+    exec = "${nixGL}/bin/nixGL ${aerie-bin}/bin/aerie";
+  };
+
+  aerie-app = pkgs.buildEnv {
+    # all launchers
+    name = "aerie-app";
+    paths = [
+      aerie-bin
+      aerie-runner
+      desktop
+    ];
   };
 }
---
+++ shell.nix
@@ -0,0 +1,44 @@
+{
+  _workspace ? import ./. {},
+  pkgs ? _workspace.pkgs,
+  libraries ? _workspace.libraries,
+  rust-toolchain ? _workspace.rust-toolchain,
+  qdrant-network ? "bridge", # For some reason, the default is broken in nix-shell
+}:
+let
+  DATA_DIR = "${builtins.getEnv "HOME"}/.local/state/refoliate";
+
+  mcp-inspect = pkgs.writeShellApplication {
+    name = "mcp-inspect";
+    runtimeInputs = with pkgs; [
+      nodejs_24
+    ];
+    text = ''
+      npx @modelcontextprotocol/inspector "$@"
+    '';
+  };
+
+  qdrant-serve = pkgs.writeShellScriptBin "qdrant-serve" ''
+    QDRANT_NETWORK=${"$"}{QDRANT_NETWORK:-${qdrant-network}}
+    ${pkgs.podman}/bin/podman run -it --rm --network=$QDRANT_NETWORK -p 6333:6333 -p 6334:6334 -v "${DATA_DIR}/qdrant:/qdrant/storage:z" qdrant/qdrant
+  '';
+in pkgs.mkShell {
+  inherit DATA_DIR;
+
+  LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath libraries}";
+  packages = with pkgs; [
+    qdrant-serve
+    mcp-inspect
+      niv
+      cmake
+      ninja
+      cargo-generate
+      mdbook
+      mdbook-d2
+      moon
+      nodejs_22
+      pkg-config
+      rust-toolchain
+      uv
+  ] ++ libraries;
+}
