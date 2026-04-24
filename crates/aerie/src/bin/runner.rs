e16aa2ac55580577c0f17c634f4d2780885f7ba1
patonw <4967614-patonw@users.noreply.gitlab.com>
2026-04-24 11:22:25 -0700

    Refactor runner by extracting run loop into a function

--- crates/aerie/src/bin/runner.rs
+++ crates/aerie/src/bin/runner.rs
@@ -1,4 +1,12 @@
-use std::{fs::OpenOptions, path::PathBuf, sync::Arc};
+use std::{
+    convert::identity,
+    fs::OpenOptions,
+    path::{Path, PathBuf},
+    sync::{
+        Arc, Mutex,
+        atomic::{AtomicUsize, Ordering},
+    },
+};
 
 use aerie::{
     AgentFactory, ChatSession, Settings,
@@ -7,7 +15,7 @@ use aerie::{
     toolbox::ToolStore,
     utils::{ImageResolver, message_text},
     workflow::{
-        RootContext, RunContext, Workflow,
+        RootContext, RunContext, Value, Workflow,
         runner::WorkflowRunner,
         store::{WorkflowStore as _, WorkflowStoreDir},
         write_value,
@@ -16,41 +24,63 @@ use aerie::{
 use arc_swap::{ArcSwap, ArcSwapOption};
 use clap::{Parser, Subcommand};
 use egui_snarl::Snarl;
-use serde::Serializer as _;
+use serde::{Deserialize, Serialize, Serializer as _};
 use serde_json::json;
+use serde_with::skip_serializing_none;
 use serde_yaml_ng as serde_yml;
+use tokio::{runtime::Runtime, task::JoinHandle};
 use tracing_subscriber::{EnvFilter, fmt, prelude::*};
+use typed_builder::TypedBuilder;
 
-#[derive(clap::Args, Clone, Debug)]
+#[skip_serializing_none]
+#[derive(clap::Args, Clone, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct ExecArgs {
     /// The workflow file to run
-    workflow: PathBuf,
+    workflow: String,
 
     /// Initial user prompt if required by the workflow
+    #[serde(default)]
     #[arg(short, long, visible_alias("prompt"))]
     input: Option<String>,
 
     /// Path to file containing the initial prompt
+    #[serde(default)]
     #[arg(short = 'I', long)]
     input_file: Option<PathBuf>,
 
     /// Either a file path or data url
-    #[arg(long, action = clap::ArgAction::Append)]
-    image: Vec<String>,
+    #[serde(default, skip_serializing_if = "Vec::is_empty")]
+    #[arg(long, visible_alias("image"), action = clap::ArgAction::Append)]
+    images: Vec<String>,
 
     /// Save outputs as individual files in a directory
+    #[serde(default)]
     #[arg(short, long)]
     out_dir: Option<PathBuf>,
 
     /// Number of extra turns to run chained workflows
+    #[serde(default, skip_serializing_if = "is_zero")]
     #[arg(short, long, default_value_t = 0)]
     autoruns: usize,
 
     /// Prints an additional object containing the next workflow after the last run
+    #[serde(default, skip_serializing_if = "std::ops::Not::not")]
     #[arg(short = 'n', long, action = clap::ArgAction::SetTrue, default_value_t = false)]
     show_next: bool,
+
+    /// Include workflow ids in output
+    #[serde(default, skip_serializing_if = "std::ops::Not::not")]
+    #[arg(short = 'n', long, action = clap::ArgAction::SetTrue, default_value_t = false)]
+    show_ids: bool,
 }
 
+#[inline]
+fn is_zero(value: &usize) -> bool {
+    *value == 0
+}
+
+#[non_exhaustive]
 #[derive(Subcommand, Clone, Debug)]
 pub enum Command {
     Exec(ExecArgs),
@@ -119,35 +149,47 @@ fn main() -> anyhow::Result<()> {
         };
     }
 
-    let Command::Exec(ExecArgs {
-        workflow,
-        input,
-        input_file,
-        image,
-        out_dir,
-        autoruns,
-        show_next,
-    }) = &args.command;
-
-    if *autoruns > 0 && args.workflows.is_none() {
-        anyhow::bail!("Cannot use autorun without a workflow store");
+    match &args.command {
+        Command::Exec(exec_args) => execute_workflow(&args, exec_args)?,
     }
 
-    if let Some(out_dir) = &out_dir {
-        std::fs::create_dir_all(out_dir)?;
-    }
+    Ok(())
+}
 
-    let mut workflow_store = args
+#[derive(TypedBuilder)]
+struct ExecOverrides {
+    #[builder(default=Arc::new(identity))]
+    pub run_ctx_fn: Arc<dyn Fn(RunContext) -> RunContext + Send + Sync>,
+
+    #[builder(default=Arc::new(|| {}))]
+    pub post_exec_fn: Arc<dyn Fn() + Send + Sync>,
+}
+
+impl Default for ExecOverrides {
+    fn default() -> Self {
+        Self::builder().build()
+    }
+}
+
+struct Scaffolding {
+    pub workflow_store: Option<WorkflowStoreDir>,
+    pub session: ChatSession,
+    pub settings: Settings,
+    pub rt: Runtime,
+    pub agent_factory: AgentFactory,
+}
+
+fn make_scaffolding(args: &Args) -> anyhow::Result<Scaffolding> {
+    let workflow_store = args
         .workflows
+        .as_ref()
         .map(|p| WorkflowStoreDir::load_all(p, false))
         .transpose()?;
-
-    let tool_store = args.tools.map(|p| {
+    let tool_store = args.tools.as_ref().map(|p| {
         let store = ToolStore::new(p);
         store.preload_all();
         store
     });
-
     let session_dir = args
         .session
         .as_ref()
@@ -164,7 +206,6 @@ fn main() -> anyhow::Result<()> {
     if let Some(branch) = &args.branch {
         session.transform(|history| Ok(history.switch(branch)))?;
     }
-
     let settings_path = if let Some(path) = &args.config {
         if !path.is_file() {
             anyhow::bail!("Configuration file does not exist: {path:?}");
@@ -177,30 +218,26 @@ fn main() -> anyhow::Result<()> {
         //     .unwrap_or_default()
         //     .join("workbench.yml")
     };
-
-    // Runtime settings:
     let mut settings = if settings_path.is_file() {
         let text = std::fs::read_to_string(&settings_path)?;
         serde_yml::from_str(&text)?
     } else {
         Settings::default()
     };
-
     tracing::debug!("Loaded settings {settings:?}");
-
     if let Some(model) = &args.model {
         settings.llm_model = model.clone();
     }
-
     if let Some(temperature) = &args.temperature {
         settings.temperature = *temperature;
     }
-
     let rt = tokio::runtime::Builder::new_multi_thread()
         .worker_threads(4)
         .enable_all()
         .build()?;
 
+    // The chain tool requires these. It's an impediment to concurrent execution.
+    // Need to scope this by run somehow
     let next_workflow: Arc<ArcSwapOption<String>> = Default::default();
     let next_prompt: Arc<ArcSwapOption<String>> = Default::default();
     let next_images: Arc<ArcSwap<Vec<String>>> = Default::default();
@@ -214,22 +251,56 @@ fn main() -> anyhow::Result<()> {
         .next_prompt(next_prompt.clone())
         .next_images(next_images.clone())
         .build();
-
     agent_factory.reload_tools()?;
 
     // TODO: better synchronization mechanism for waiting on tools to load
     loop {
-        std::thread::sleep(std::time::Duration::from_millis(100));
+        std::thread::sleep(std::time::Duration::from_millis(500));
 
-        let num_tasks = agent_factory
-            .task_count
-            .load(std::sync::atomic::Ordering::Relaxed);
+        let num_tasks = agent_factory.task_count.load(Ordering::Relaxed);
         if num_tasks < 1 {
             break;
         }
 
         tracing::info!("Waiting for {num_tasks} tools to load...");
     }
+
+    // let overrides = ExecOverrides {
+    //     settings_fn: Arc::new(move |mut settings| {
+    //         settings.llm_model = format!("{:?}", next_workflow);
+    //         settings
+    //     }),
+    // };
+
+    Ok(Scaffolding {
+        workflow_store,
+        session,
+        settings,
+        rt,
+        agent_factory,
+    })
+}
+
+fn execute_workflow(args: &Args, exec_args: &ExecArgs) -> anyhow::Result<()> {
+    let ExecArgs {
+        workflow,
+        input,
+        input_file,
+        images: image,
+        out_dir,
+        autoruns,
+        ..
+    } = &exec_args;
+
+    if *autoruns > 0 && args.workflows.is_none() {
+        anyhow::bail!("Cannot use autorun without a workflow store");
+    }
+
+    if let Some(out_dir) = &out_dir {
+        std::fs::create_dir_all(out_dir)?;
+    }
+
+    let mut scaffolding = make_scaffolding(args)?;
 
     let mut prompt = input.as_ref().cloned().unwrap_or_default();
     if &prompt == "-" {
@@ -240,19 +311,127 @@ fn main() -> anyhow::Result<()> {
         prompt = std::fs::read_to_string(path)?;
     }
 
-    let mut images = image.clone();
-    let workflow_path = workflow.as_path();
+    let images = image.clone();
+    let workflow_path = Path::new(workflow);
 
-    let mut shadow: Workflow = if workflow_path.is_file() {
+    let graph: Workflow = if workflow == "__default__" {
+        Default::default()
+    } else if workflow_path.is_file() {
         let reader = OpenOptions::new().read(true).open(workflow_path)?;
         serde_yml::from_reader(reader)?
-    } else if let Some(store) = &mut workflow_store {
-        store.load(&workflow.display().to_string())?
+    } else if let Some(store) = &mut scaffolding.workflow_store {
+        store.load(workflow)?
     } else {
-        anyhow::bail!("Invalid file: {workflow_path:?}");
+        anyhow::bail!("Invalid file: {workflow:?}");
     };
 
-    for run_count in 0..=*autoruns {
+    let run_count = Arc::new(AtomicUsize::default());
+
+    let saver_task: Arc<Mutex<Option<JoinHandle<anyhow::Result<()>>>>> = Default::default();
+
+    let overrides = ExecOverrides::builder()
+        .run_ctx_fn({
+            let rt = scaffolding.rt.handle().clone();
+            let run_count = run_count.clone();
+            let saver_task = saver_task.clone();
+            let out_dir = out_dir.clone();
+            let autoruns = *autoruns;
+
+            Arc::new(move |run_ctx| {
+                let receiver = run_ctx.outputs.receiver();
+                let task = if let Some(out_dir) = &out_dir {
+                    let out_dir = if autoruns > 0 {
+                        out_dir.join(run_count.load(Ordering::Relaxed).to_string())
+                    } else {
+                        out_dir.clone()
+                    };
+
+                    rt.spawn(file_output(receiver, out_dir))
+                } else {
+                    rt.spawn(console_output(receiver))
+                };
+
+                *saver_task.lock().unwrap() = Some(task);
+
+                run_count.fetch_add(1, Ordering::Relaxed);
+                run_ctx
+            })
+        })
+        .post_exec_fn({
+            let rt = scaffolding.rt.handle().clone();
+            let saver_task = saver_task.clone();
+
+            Arc::new(move || {
+                let mut saver_task = saver_task.lock().unwrap();
+
+                if let Some(saver_task) = saver_task.take() {
+                    rt.block_on(async move {
+                        match saver_task.await {
+                            Ok(Ok(_)) => {}
+                            Err(err) => {
+                                tracing::warn!("{err:?}");
+                            }
+                            Ok(Err(err)) => {
+                                tracing::warn!("{err:?}");
+                            }
+                        }
+                    });
+                }
+            })
+        })
+        .build();
+
+    run_loop(&scaffolding, exec_args, overrides, prompt, images, graph)?;
+
+    if args.update_session && args.session.is_some() {
+        scaffolding.session.save()?;
+    }
+
+    let agent_factory = &scaffolding.agent_factory;
+    let next_workflow = agent_factory.next_workflow.clone();
+    let next_prompt = agent_factory.next_prompt.clone();
+    let next_images = agent_factory.next_images.clone();
+
+    if exec_args.show_next {
+        let next_workflow = next_workflow
+            .swap(Default::default())
+            .map(|s| s.as_ref().clone());
+        let next_prompt = next_prompt
+            .swap(Default::default())
+            .map(|s| s.as_ref().clone());
+        let next_images = next_images.swap(Default::default()).as_ref().clone();
+        let blob = json!({
+            "next_workflow": next_workflow,
+            "next_prompt": next_prompt,
+            "next_images": next_images,
+        });
+
+        println!("{blob}");
+    }
+
+    Ok(())
+}
+
+fn run_loop(
+    scaffolding: &Scaffolding,
+    exec_args: &ExecArgs,
+    overrides: ExecOverrides,
+    mut prompt: String,
+    mut images: Vec<String>,
+    mut workflow: Workflow,
+) -> Result<(), anyhow::Error> {
+    let autoruns = exec_args.autoruns;
+
+    for run_count in 0..=autoruns {
+        let Scaffolding {
+            workflow_store,
+            session,
+            settings,
+            rt,
+            agent_factory,
+            ..
+        } = scaffolding;
+
         let mut extra_content = Vec::new();
         for image in &images {
             let image = aerie::rig::message::Image {
@@ -272,30 +451,18 @@ fn main() -> anyhow::Result<()> {
 
         let run_ctx = RunContext::builder()
             .runtime(rt.handle().clone())
-            .exec_id(shadow.graph.uuid.into())
+            .exec_id(workflow.graph.uuid.into())
             .agent_factory(agent_factory.clone())
-            .metadata(shadow.metadata.clone())
+            .metadata(workflow.metadata.clone())
             .history(session.history.clone())
             .seed(settings.seed.clone())
             .build();
 
-        let saver_task = if let Some(out_dir) = out_dir {
-            let out_dir = if *autoruns > 0 {
-                out_dir.join(run_count.to_string())
-            } else {
-                out_dir.clone()
-            };
-
-            rt.handle()
-                .spawn(file_output(run_ctx.outputs.receiver(), out_dir))
-        } else {
-            rt.handle()
-                .spawn(console_output(run_ctx.outputs.receiver()))
-        };
-
+        let run_ctx = (overrides.run_ctx_fn)(run_ctx);
+        let out_rx = run_ctx.outputs.sender();
         let inputs = RootContext::builder()
             .history(session.history.clone())
-            .workflow(shadow.clone())
+            .workflow(workflow.clone())
             .user_prompt(std::mem::take(&mut prompt))
             .extra_content(extra_content)
             .model(settings.llm_model.clone())
@@ -308,8 +475,8 @@ fn main() -> anyhow::Result<()> {
             .run_ctx(run_ctx)
             .build();
 
-        exec.init(&shadow.graph);
-        let mut snarl = Snarl::try_from(shadow.graph.as_ref().clone())?;
+        exec.init(&workflow.graph);
+        let mut snarl = Snarl::try_from(workflow.graph.as_ref().clone())?;
 
         let result = loop {
             match exec.step(&mut snarl) {
@@ -322,59 +489,48 @@ fn main() -> anyhow::Result<()> {
             }
         };
 
+        if exec_args.show_ids {
+            out_rx
+                .send((
+                    "__id".into(),
+                    Value::Text(Arc::new(workflow.graph.uuid.0.to_string())),
+                ))
+                .expect("Problem sending graph id");
+            out_rx
+                .send(("__run".into(), Value::Integer(1 + run_count as i64)))
+                .expect("Problem sending graph id");
+        }
+
+        drop(out_rx);
         drop(exec);
 
-        rt.block_on(async move {
-            match saver_task.await {
-                Ok(Ok(_)) => {}
-                Err(err) => {
-                    tracing::warn!("{err:?}");
-                }
-                Ok(Err(err)) => {
-                    tracing::warn!("{err:?}");
-                }
-            }
-        });
+        (overrides.post_exec_fn)();
 
         result?;
 
-        if args.update_session && args.session.is_some() {
-            session.save()?;
-        }
-
-        if run_count < *autoruns {
-            if let Some(next_prompt) = next_prompt.swap(Default::default()) {
+        if run_count < autoruns {
+            if let Some(next_prompt) = agent_factory.next_prompt.swap(Default::default()) {
                 prompt = next_prompt.as_ref().to_owned();
             }
 
             // TODO: efficiency
-            images = next_images.swap(Default::default()).as_ref().clone();
+            images = agent_factory
+                .next_images
+                .swap(Default::default())
+                .as_ref()
+                .clone();
 
-            if let Some(next_workflow) = next_workflow.swap(Default::default()).as_ref()
-                && let Some(store) = &mut workflow_store
+            if let Some(next_workflow) = agent_factory
+                .next_workflow
+                .swap(Default::default())
+                .as_ref()
+                && let Some(store) = workflow_store
             {
-                shadow = store.load(next_workflow)?;
+                workflow = store.load(next_workflow)?;
             } else {
                 break;
             }
         }
-    }
-
-    if *show_next {
-        let next_workflow = next_workflow
-            .swap(Default::default())
-            .map(|s| s.as_ref().clone());
-        let next_prompt = next_prompt
-            .swap(Default::default())
-            .map(|s| s.as_ref().clone());
-        let next_images = next_images.swap(Default::default()).as_ref().clone();
-        let blob = json!({
-            "next_workflow": next_workflow,
-            "next_prompt": next_prompt,
-            "next_images": next_images,
-        });
-
-        println!("{blob}");
     }
 
     Ok(())
