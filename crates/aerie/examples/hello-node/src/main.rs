934455081f5f38642342048b81db8737ffe924ef
patonw <4967614-patonw@users.noreply.gitlab.com>
2026-07-29 11:47:04 -0700

    Update example project for custom node

--- crates/aerie/examples/hello-node/src/main.rs
+++ crates/aerie/examples/hello-node/src/main.rs
@@ -16,7 +16,7 @@ fn main() -> anyhow::Result<()> {
         dirs::config_dir()
             .map(|p| p.join("aerie"))
             .unwrap_or_default()
-            .join("workbench.yml"),
+            .join("settings.toml"),
     );
 
     let app = aerie::app::App::builder()
@@ -25,15 +25,6 @@ fn main() -> anyhow::Result<()> {
         .args(args)
         .settings_path(settings_path)
         .min_size(egui::vec2(800.0, 400.0))
-        // .settings_fn(Rc::new(|settings| aerie::Settings {
-        //     automation: None, // don't load last workflow
-        //     ..settings
-        // }))
-        // .appstate_fn(Rc::new(|mut state| {
-        //     // Set an initial prompt
-        //     state.prompt = "What is a prompt?".into();
-        //     state
-        // }))
         .build();
     app.run_app()?;
 
@@ -43,12 +34,13 @@ fn main() -> anyhow::Result<()> {
 #[derive(Debug, Clone, Default, Hash, PartialEq, Eq, Deserialize, Serialize)]
 pub struct HelloNode {}
 
-impl DynNode for HelloNode {}
-impl UiNode for HelloNode {
+impl DynNode for HelloNode {
     fn title(&self) -> &str {
         "Hello World"
     }
 }
+
+impl UiNode for HelloNode {}
 
 #[typetag::serde]
 impl FlexNode for HelloNode {}
@@ -56,7 +48,7 @@ impl FlexNode for HelloNode {}
 fn script_node_menu(ui: &mut egui::Ui, snarl: &mut snarl::Snarl<WorkNode>, pos: egui::Pos2) {
     ui.menu_button("hello", |ui| {
         if ui.button("Hello").clicked() {
-            snarl.insert_node(pos, HelloNode::default().into());
+            snarl.insert_node(pos.into(), HelloNode::default().into());
         }
     });
 }
